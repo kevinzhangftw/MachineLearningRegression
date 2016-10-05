38373b86ff311b23d23f23c5651bae18b58439fc
Kevin Zhang <kevinctzhang@gmail.com>
2016-10-04 20:23:48 -0700

    for loop

--- polynomial_regression_reg.py
+++ polynomial_regression_reg.py
@@ -53,7 +53,6 @@ def regularizedValidationError(training_set, training_targets, validation_set, v
 
 #TODO derive weights with regularizer 
 def gettingRegWeights(designMatrix, targets, lamb):
-	#w = (λI + Φ^T * Φ)^-1*Φ^T*t
 	designMatrixTranspose = designMatrix.T
 	designMatrixSquare = np.dot(designMatrixTranspose, designMatrix)
 	I = np.identity(designMatrixSquare.shape[0])
@@ -72,15 +71,84 @@ def gettingRegularizedRMS(features_TRAIN, target_TRAIN, lamb,features_VAL, targe
 	regularizedRMS = np.sqrt((regularizedDiff.T*regularizedDiff)/designMatrix.shape[0])
 	return regularizedRMS.item()
 
-#TODO: split input features into training features and validation feature...
-features_TRAIN = features_INPUT[:90,:]
-target_TRAIN = target_INPUT[:90,:]
-features_VAL = features_INPUT[90:N_TRAIN,:]
-target_VAL = target_INPUT[90:N_TRAIN,:]
+#set 1
+features_TRAIN = features_INPUT[10:N_TRAIN,:]
+target_TRAIN = target_INPUT[10:N_TRAIN,:]
+
+features_VAL = features_INPUT[:10,:]
+target_VAL = target_INPUT[:10,:]
+#wish: to split into 10 validation sets, and 10 features set
+
+#set2
+features_TRAINup = features_INPUT[0:10,:]
+features_TRAINdown = features_INPUT[20:N_TRAIN,:]
+
+target_TRAINup = target_INPUT[0:10,:]
+target_TRAINdown = target_INPUT[20:N_TRAIN,:]
+#concatenate features together
+features_TRAIN1 = np.concatenate((features_TRAINup,features_TRAINdown),axis=0)
+target_TRAIN1 = np.concatenate((target_TRAINup,target_TRAINdown),axis=0)
+
+features_VAL = features_INPUT[10:20,:]
+target_VAL = target_INPUT[10:20,:]
+
+#set3
+features_TRAINup = features_INPUT[0:20,:]
+features_TRAINdown = features_INPUT[30:N_TRAIN,:]
+
+target_TRAINup = target_INPUT[0:20,:]
+target_TRAINdown = target_INPUT[30:N_TRAIN,:]
+#concatenate features together
+features_TRAIN1 = np.concatenate((features_TRAINup,features_TRAINdown),axis=0)
+target_TRAIN1 = np.concatenate((target_TRAINup,target_TRAINdown),axis=0)
+
+features_VAL = features_INPUT[20:30,:]
+target_VAL = target_INPUT[20:30,:]
+
+
+#init matrices
+target_TRAINup = np.zeros((10,90,1))
+target_TRAINdown = np.zeros((10,90,1))
+#concatenate features together
+features_TRAIN1 = np.zeros((10,90,33))
+target_TRAIN1 = np.zeros((10,10,1))
+
+features_VAL = np.zeros((10,10,33))
+target_VAL = np.zeros((10,10,1))
+for i in xrange(0,10):
+	features_TRAINup = np.zeros((10,(i*10),33))
+	features_TRAINup[i] = features_INPUT[0:(i*10),:]
+
+	
+	features_TRAINdown[i] = features_INPUT[(i+1)*10:N_TRAIN,:]
+	features_TRAIN[i] = np.concatenate((features_TRAINup[i],features_TRAINdown[i]),axis=0)
+
+
+
+	target_TRAIN[i] = np.concatenate((target_TRAINup,target_TRAINdown),axis=0)
+
+
+	features_VAL[i] = features_INPUT[(i*10):(i+1)*10,:]
+	target_VAL[i] = target_INPUT[(i*10):(i+1)*10,:]
+
 
 lamb =1
+#wish: lamba from = {10^-3, 10^-2, 10^-1, 10^0, 10^1, 10^2, 10^3, 10^4}
+for i in xrange(1,9):
+	if i==1:
+		lamb = 0
+		#get regularizedRMSRow array when lamb = 0
+
+	else:
+		lamb = 10**(i-4)
+		#get regularizedRMSRow array when lamb = 10^-2, 10^-1, 10^0, 10^1, 10^2, 10^3, 10^4}
+
 
 regularizedRMSRow = np.zeros(10)
 regularizedRMSRow[0] = gettingRegularizedRMS(features_TRAIN, target_TRAIN, lamb,features_VAL, target_VAL)
 
+
+
+#RMSRow is array of 10, first position is regularizedRMSRow[0], rest of 9 are 0s
+avgRegularizedRMSRow = np.average(regularizedRMSRow)
 
